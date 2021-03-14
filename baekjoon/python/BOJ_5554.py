4fd82447749bfbf11653780fecafc5af230d7190
Hyunseok-kim <ai9gle@gmail.com>
2021-03-14 16:27:51 +0900

    feat(Hyunseok-Kim): [BOJ, 5554] 심부름 가는길

--- baekjoon/python/BOJ_5554.py
+++ baekjoon/python/BOJ_5554.py
@@ -1,8 +1,8 @@
-# https://www.acmicpc.net/problem/5522
+# https://www.acmicpc.net/problem/5554
 a = 0
 a += int(input())
 a += int(input())
 a += int(input())
 a += int(input())
-a += int(input())
-print(a)
+print(int(a/60))
+print(int(a%60))
