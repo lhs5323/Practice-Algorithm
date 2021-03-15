d0d2d789ea6700a9f43bbb45463735fe9722d4d8
rladbwls027 <80633131+rladbwls027@users.noreply.github.com>
2021-03-15 20:35:03 +0900

    feat(Yoojin-Kim): [BOJ, 14645] 와이버스 부릉부릉 https://www.acmicpc.net/problem/14645

---
+++ baekjoon/basic 100/14645.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k; cin >> n >> k;
+    int a, b;
+    
+    while(n-- > 0){
+        scanf("%d%d", &a, &b);
+    }
+    
+    cout << "비와이";
+    
+    return 0;
+}
