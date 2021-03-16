f01363862691c7653bfd53cfebd5455e11896970
rladbwls027 <80633131+rladbwls027@users.noreply.github.com>
2021-03-16 14:28:05 +0900

    feat(Yoojin-Kim): [BOJ, 18108] 1998년생인 내가 태국에서는 2541년생?!
    https://www.acmicpc.net/problem/18108

---
+++ baekjoon/basic 100/BOJ 18108.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int boolgi, yungi;
+    cin >> boolgi;
+    
+    yungi = boolgi - 543;
+    
+    cout << yungi;
+    
+    return 0;
+}
