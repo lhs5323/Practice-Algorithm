7605f310e1753996ed3ec4c937f8635f3d4d163e
Mr.k <bcgrhio@gmail.com>
2021-03-22 21:25:27 +0900

    feat(Hyoha): [BOJ, 2753] 윤년 https://www.acmicpc.net/problem/2753

---
+++ baekjoon/2753.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] agrs) throws IOException{
+			Scanner sc = new Scanner(System.in);
+			int year = sc.nextInt();
+			if(year%4==0 && (year%100!=0 || year%400==0))
+				System.out.println(0);
+			else
+				System.out.println(1);
+	}
+}
